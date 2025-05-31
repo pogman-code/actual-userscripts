9bca6651722fd0bbc539717b6c6b90a91df483a2
POGMAN <adrian.maurin@gmail.com>
2025-05-31 17:29:10 +0200

    make balance colors dynamic if it changes

--- actual-balancecolors/index.js
+++ actual-balancecolors/index.js
@@ -1,11 +1,13 @@
 const setAccountBalanceColors = () => {
   var searchPattern = /^\-\d+/;
-  
+
   for (const b of document.querySelectorAll(
     "span[data-testid^='__global!balance-']:not([data-testid^='__global!balance-query']), span[data-testid$='-balance']"
   )) {
     if (searchPattern.test(b.textContent)) {
-      b.style.color = 'var(--color-errorText)';
+      b.classList.add("error");
+    } else {
+      b.classList.remove("error");
     }
   }
 };
